c0265659dab3eb02c120e7d1f3f69623e3e982b2
monipeny <mlamasrial@gmail.com>
2022-05-25 16:00:15 +0200

    fix dependencies

--- src/test/java/com/cucumber/stepdefs/UsersImplementation.java
+++ src/test/java/com/cucumber/stepdefs/UsersImplementation.java
@@ -1,6 +1,7 @@
 package com.cucumber.stepdefs;
 
 import static io.restassured.RestAssured.given;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasKey;
@@ -28,5 +29,25 @@ public class UsersImplementation implements Serializable {
         RestAssured.baseURI = "https://reqres.in/api/";
     }
 
+    @Given("the following get request that brings us the users")
+    public Response getUsers(){
+        //Response responseGetUsers = given().baseUri("https://reqres.in/api/users?page=2").get();
+        // given().param("page", 2).baseUri("https://reqres.in/api/users").get();
+        Response responseGetUsers = given().log().all().param("page", 2).get("/users");
+        return responseGetUsers;
+    }
 
+    @And("the response is 200")
+    public void validateResponse(){
+        assertTrue("The response is not 200",getUsers().statusCode()==200);
+    }
+
+    @And("the body response contains the corresponding ids")
+    public void validateUserIds(){
+        getUsers().then().body("data.id", hasItems(7,8,9,10,11,12));
+    }
+    @Then("the total page contains {int}")
+    public void validateTotalPages(Integer page){
+        getUsers().then().body("total_pages", equalTo(page));
+    }
 }
