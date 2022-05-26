d0ebee2187e2f505f768bf8aa21a1c4921145803
monipeny <mlamasrial@gmail.com>
2022-05-26 12:00:05 +0200

    getAvailable successful feature

--- src/test/java/com/cucumber/stepdefs/PetsImplementation.java
+++ src/test/java/com/cucumber/stepdefs/PetsImplementation.java
@@ -1,22 +1,16 @@
 package com.cucumber.stepdefs;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.messages.internal.com.fasterxml.jackson.databind.JsonNode;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import java.io.Serializable;
-import java.util.HashMap;
 
 
 public class PetsImplementation implements Serializable {
@@ -26,6 +20,7 @@ public class PetsImplementation implements Serializable {
         RestAssured.baseURI = "https://petstore.swagger.io/v2/";
     }
 
+ // Get list All pets
     @Given("the following get request that brings us the pets list")
     public Response getAllPets(){
 
@@ -36,4 +31,22 @@ public class PetsImplementation implements Serializable {
     public void validateResponse(){
         assertTrue("The response is not 200",getAllPets().statusCode()==200);
     }
+
+
+// Get list pets availables
+    @Given("the following get request that brings us the pets availables")
+    public Response listPetAvailable() {
+         Response responseListPetAvalible = given().param("status", "availables").get("/pet/findByStatus");
+         return responseListPetAvalible;
+    }
+    /*@And("response message")
+    public void validateMessageResponse(){
+        listPetAvailable().then().body("data",contains("status", "available") );
+    }*/
+    @Then("the response is 200 for avalaibles")
+    public void validateResponseAvalaibles(){
+        assertTrue("The response is not 200",listPetAvailable().statusCode()==200);
+    }
+
+
 }
--- target/classes/pets.feature
+++ target/classes/pets.feature
@@ -1,6 +1,14 @@
 Feature: (e2e) Validate pets
 
-  @pets
+  @petsAll
   Scenario: (e2e) Validate that the response of the pets request is 200
     Given the following get request that brings us the pets list
     Then the response is 200
+
+  @petsAvailable
+  Scenario: (e2e) Validate that pets are in available status
+    Given the following get request that brings us the pets availables
+#    And response message
+    Then the response is 200 for avalaibles
+
+
